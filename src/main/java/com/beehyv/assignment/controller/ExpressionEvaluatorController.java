608edc7580819ef5a108f99a69b8304f18528ee2
SModi <modisanket123@gmail.com>
2021-05-16 19:43:05 +0530

    Improved api documentation

--- src/main/java/com/beehyv/assignment/controller/ExpressionEvaluatorController.java
+++ src/main/java/com/beehyv/assignment/controller/ExpressionEvaluatorController.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +22,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @RestController
+@Tag(name = "Expression evaluator")
 public class ExpressionEvaluatorController {
 
     private final ExpressionEvaluatorService service;
